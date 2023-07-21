9bef71d0c9b903370aed3c328ac38f456f90bcac
adityagitproject <140141541+adityagitproject@users.noreply.github.com>
2023-07-21 10:58:16 +0530

    Add files via upload

---
+++ webpagedesigne.js
@@ -0,0 +1,7 @@
+ console.log('script loaded')
+
+ console.log(document.getElementById("logo"))
+
+console.log(document.getElementsByClassName("expert-div"))
+
+console.log(document.getElementsByClassName("expert-item"))
